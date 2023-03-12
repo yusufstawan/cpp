36b55d7f22f4f5e5e6356e982711d8f968b88dee
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-12 09:06:00 +0700

    variable

---
+++ 14.deklarasi_dan_inisialisasi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int hasil = 10;
+  double ipk = 3.5;
+  char kelas = 'A';
+  string kegiatan = "Belajar C++";
+
+  cout << hasil;
+  cout << "\n";
+  cout << ipk;
+  cout << "\n";
+  cout << kelas;
+  cout << "\n";
+  cout << kegiatan;
+  cout << "\n";
+
+  return 0;
+}
---
+++ 15.mengubah_nilai_variable.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int harga_barang = 15000;
+  cout << harga_barang;
+  cout << "\n";
+
+  harga_barang = 20000;
+  cout << harga_barang;
+  cout << "\n";
+
+  harga_barang = 30000;
+  cout << harga_barang;
+  cout << "\n";
+
+  return 0;
+}
