6d3a7eefc379d320dd0bd97984b1b57d380c0496
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-12 10:46:15 +0700

    cout

---
+++ 18.cout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a = 5000;
+
+  cout << "Isi dari variabel a adalah: " << a << '\n';
+
+  return 0;
+}
---
+++ 19.cout_endl.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Belajar C++" << endl;
+  cout << "Sangat Mudah" << endl;
+}
