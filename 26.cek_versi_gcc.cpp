7f418ac20766ef80c528a742494a04700c41d57a
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-18 03:55:10 +0700

    integer positif & negatif

---
+++ 26.cek_versi_gcc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << __cplusplus;
+  return 0;
+}
---
+++ 28.integer_negatif.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  short a = -32767;
+  int b = -2147483647;
+  long c = -2147483647;
+  long long d = -9223372036854775807;
+
+  cout << a << endl;
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+
+  return 0;
+}
